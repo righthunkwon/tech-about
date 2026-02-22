26320f586e56638bbf6159d71d596e115b2705f7
kwonjeonghun <114549688+righthunkwon@users.noreply.github.com>
2026-02-23 00:28:14 +0900

    feat: 공통 버튼 컴포넌트 개발

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,14 +1,48 @@
 import React from 'react';
 
-const Button: React.FC = () => {
+interface props {
+  children: React.ReactNode;
+  variant?: 'default' | 'outline';
+  onClick?: () => void;
+  className?: string;
+  disabled?: boolean;
+}
+
+const Button: React.FC<props> = ({
+  children,
+  variant = 'default',
+  onClick,
+  className = '',
+  disabled = false,
+}) => {
+  /**
+   * 버튼 디자인을 가져오는 함수
+   * @returns string
+   */
+  const getButtonStyle = () => {
+    if (disabled) {
+      return `ta-button-disabled ${className}`;
+    }
+
+    switch (variant) {
+      case 'outline':
+        return `ta-button-outline ${className}`;
+      default:
+        return `ta-button-default ${className}`;
+    }
+  };
+  const buttonStyle = getButtonStyle();
+
+  /**
+   * Button
+   * - disabled의 경우 속성 명시 필수
+   * - 명시하지 않을 시 개발자도구에서 스타일 제거하면 버튼 클릭 가능
+   */
   return (
     <>
-      <button
-        // onClick={onClickSave}
-        className="bg-ta-blue hover:bg-ta-blue-dark rounded-lg px-4 py-2 text-white hover:cursor-pointer"
-      >
-        버튼
-      </button>{' '}
+      <button className={buttonStyle} onClick={onClick} disabled={disabled}>
+        {children}
+      </button>
     </>
   );
 };
